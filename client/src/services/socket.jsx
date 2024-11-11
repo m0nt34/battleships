264cc8b35291ab230d01b5300738e59287cdc08d
m0nt34 <116570394+m0nt34@users.noreply.github.com>
2024-11-11 18:49:38 +0400

    online server update

--- client/src/services/socket.jsx
+++ client/src/services/socket.jsx
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000"); 
+const socket = io("https://battleships-z8bh.onrender.com"); 
 
 const connectSocket = () => {
     if (!socket.connected) {
--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://battleships-z8bh.onrender.com",
     credentials: true,
   },
 });
