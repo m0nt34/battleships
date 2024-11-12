19b637b2e9e5c21660e8a425b6a4615aa6c557a8
m0nt34 <116570394+m0nt34@users.noreply.github.com>
2024-11-12 18:25:33 +0400

    searching users ship position bug fix

--- client/src/components/Buttons.jsx
+++ client/src/components/Buttons.jsx
@@ -4,15 +4,17 @@ import Reset from "../assets/Icons/Reset";
 import { handleReset } from "../utils/handleReset";
 import { randomize } from "../utils/RandomShipPlacement";
 import { useGame } from "../store/useGame";
+import { useLoading } from "../store/useLoading";
 const Buttons = () => {
   const { gameStarted } = useGame();
-
+  const { loading } = useLoading();
   return (
     <div className="h-6">
       {!gameStarted && (
         <div className="flex w-full justify-center gap-5 mt-5">
           <button
             onClick={randomize}
+            disabled={loading}
             className="flex items-center justify-center text-xl gap-1"
           >
             <Randomize />
@@ -20,6 +22,7 @@ const Buttons = () => {
           </button>
           <button
             onClick={handleReset}
+            disabled={loading}
             className="flex items-center justify-center text-xl gap-1"
           >
             <Reset />
